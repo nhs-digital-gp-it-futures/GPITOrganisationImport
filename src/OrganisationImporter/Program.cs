308a2aea01427f90284799bb3dafca1325285b33
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2022-11-01 11:26:48 +0000

    Add logging

--- src/OrganisationImporter/Program.cs
+++ src/OrganisationImporter/Program.cs
@@ -1,18 +1,32 @@
 ﻿using CommandLine;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using OrganisationImporter.Models;
 using OrganisationImporter.Services;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+          .WriteTo.Console()
+          .CreateLogger();
 
 var services = new ServiceCollection()
-    .AddSingleton<OrganisationImportService>();
+    .AddSingleton<OrganisationImportService>()
+    .AddLogging(builder => builder.AddSerilog());
+
+var serviceProvider = services.BuildServiceProvider();
 
 Parser
     .Default
     .ParseArguments<CommandLineArgs>(args)
+    .WithNotParsed(errors =>
+    {
+        var logger = serviceProvider.GetService<ILogger<Program>>();
+
+        logger.LogError("Failed to parse command line args.");
+    })
     .WithParsedAsync(parsedArgs =>
     {
-        var organisationImportService = services
-            .BuildServiceProvider()
+        var organisationImportService = serviceProvider
             .GetService<OrganisationImportService>();
 
         return organisationImportService!.ImportFromUrl(parsedArgs.Url);
--- src/OrganisationImporter/Services/OrganisationImportService.cs
+++ src/OrganisationImporter/Services/OrganisationImportService.cs
@@ -1,10 +1,19 @@
+using Microsoft.Extensions.Logging;
+
 namespace OrganisationImporter.Services;
 
 public class OrganisationImportService
 {
+    private readonly ILogger<OrganisationImportService> logger;
+
+    public OrganisationImportService(ILogger<OrganisationImportService> logger)
+    {
+        this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
     public Task ImportFromUrl(Uri url)
     {
-        Console.WriteLine($"Received {url}");
+        logger.LogInformation($"Received {url}");
 
         return Task.CompletedTask;
     }
